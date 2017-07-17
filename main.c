59bc15380b7a9b20ac082aae8ada6fe81668db51
bbqur6045 <bbqur6045@naver.com>
2017-07-17 22:55:03 +0900

    update

--- main.c
+++ main.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 #include <pcap.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
+
 
 int main(int argc, char **argv)
 {
-    char *dev;
-    char dicebuf[PCAP_ERRBUF_SIZE];
-    dev = pcap_lookupdev(dicebuf);
-    if (dev != NULL)
-    {
-        printf("%s\n", dev);
-    }
+#define NONPROMISCUOUS 0
+	char *dev;
+	char dicebuf[PCAP_ERRBUF_SIZE];
+	
+	pcap_t *pc;
+
+	dev = pcap_lookupdev(dicebuf);
+	if (dev != NULL)
+	{	
+		printf("%s\n", dev);
+	}
 	printf("%s\n", dev);
+	
+	pc= pcap_open_live(dev, BUFSIZ, NONPROMISCUOUS, -1, dicebuf);
+	if (pc == NULL)
+	{
+	printf("%s\n", dicebuf);
+	exit(1);
+	}
 }
+
