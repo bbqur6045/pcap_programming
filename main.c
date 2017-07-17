2a751c30247c36ef6b095485c7ae15fe84a3dbca
bbqur6045 <bbqur6045@naver.com>
2017-07-18 02:48:54 +0900

    update

--- main.c
+++ main.c
@@ -1,30 +1,139 @@
-#include <stdio.h>
 #include <pcap.h>
+#include <stdio.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/if_ether.h>
+#include <net/ethernet.h>
 #include <stdlib.h>
 #include <string.h>
-#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
+int main(int argc, char *argv[])
 
-int main(int argc, char **argv)
 {
-#define NONPROMISCUOUS 0
-	char *dev;
-	char dicebuf[PCAP_ERRBUF_SIZE];
-	
-	pcap_t *pc;
 
-	dev = pcap_lookupdev(dicebuf);
-	if (dev != NULL)
-	{	
-		printf("%s\n", dev);
-	}
-	printf("%s\n", dev);
+	pcap_t *handle;			/* Session handle */
+	char *dev;			/* The device to sniff on */
+	char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
+	struct bpf_program fp;		/* The compiled filter */
+	char filter_exp[] = "port 80";	/* The filter expression */
+	bpf_u_int32 mask;		/* Our netmask */
+	bpf_u_int32 net;		/* Our IP */
+	struct pcap_pkthdr header;	/* The header that pcap gives us */
+	const u_char *packet;		/* The actual packet */
+
 	
-	pc= pcap_open_live(dev, BUFSIZ, NONPROMISCUOUS, -1, dicebuf);
-	if (pc == NULL)
+
+	int i,j;
+	struct ip *iph;
+	struct tcphdr *tcph;
+	struct ether_header *ethh;
+
+
+
+	/* Define the device */
+
+	dev = pcap_lookupdev(errbuf);
+	if (dev == NULL) 
+  	{
+		fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
+		return(2);
+
+	}
+	printf("lookupdev complete\n");
+	/* Find the properties for the device */
+
+	if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) 
+    {
+		fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
+		net = 0;
+		mask = 0;
+	}
+	printf("lookupnet complete\n");
+	/* Open the session in promiscuous mode */
+
+	handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+	if (handle == NULL) {
+		fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
+		return(2);
+	}
+	printf("pcap open live complete\n");
+	/* Compile and apply the filter */
+
+	if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
+		fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
+		return(2);
+	}
+
+	if (pcap_setfilter(handle, &fp) == -1) {
+		fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
+		return(2);
+	}
+	printf("right before grabbing packet\n");
+	/* Grab a packet */
+
+	
+	while(1)
 	{
-	printf("%s\n", dicebuf);
-	exit(1);
-	}
-}
+	packet = pcap_next(handle, &header);	
+        
+	ethh =(struct ether_header *)packet;
+				
+        packet +=sizeof(struct ether_header);
+		
+        iph =(struct ip *)packet;
+		
+        packet += sizeof(struct ip);
+        
+        tcph =(struct tcphdr *)packet;
 
+	packet += sizeof(struct tcphdr);
+
+	printf("-------------------------\n");
+
+	printf("eth.smac = "); 
+        for(j=0;j<10;j++)
+	{
+		fprintf(stdout,"%02x:",ethh->ether_shost[j]); 
+	}
+	printf("\n");
+	
+	printf("eth.dmac = "); 
+        for(j=0;j<10;j++)
+	{
+	printf("%02x:",ethh->ether_dhost[j]);
+	}
+ 
+	printf("\n");
+
+	printf("SIP = %s\n",inet_ntoa(iph->ip_src));
+	printf("DIP = %s\n",inet_ntoa(iph->ip_dst));
+
+	printf("SPORT = %d\n",ntohs(tcph->th_sport));
+	printf("DPORT = %d\n",ntohs(tcph->th_dport));
+
+		
+
+	printf("DATA:\n");
+
+		for(j=0;j<(header.len)-sizeof(struct ether_header)-sizeof(struct ip)-sizeof(struct tcphdr);j++)
+        {
+
+			printf("%02x ",*packet);
+			packet++;
+
+			if(j % 20 == 0 && j!=0)
+			printf("\n");
+
+	}
+
+		printf("\n-------------------------------\n");
+
+	}
+
+	pcap_close(handle);	
+
+	return(0);
+
+ }
